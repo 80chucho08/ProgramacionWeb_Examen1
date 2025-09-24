101ff118ee13bd3e79ebcb5ff6d0bd04fb639dd8
80chucho08 <chucho2004carbajal@gmail.com>
2025-09-24 08:06:35 -0600

    primer commit

---
+++ miempresa/layoutEmpresa.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PromocionHgo</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to.fit=no">
+    <link href="bootstrap/css/bootstrap.min.css"
+        rel="stylesheet">
+  <style>
+    .titulo
+    {
+      color: white;
+    }
+    body
+    {
+      background: linear-gradient(rgba(0,0,0,0.5), rgba(0,0,0,0.5)),
+                  url("img/tepeapulco.jpg") no-repeat center center fixed;
+      background-size: cover;
+    }
+    .panel 
+    {
+        background: rgba(86, 143, 135, 0.7);
+        color: white; 
+        text-align: center;
+        padding: 15px; 
+    }
+    .izquierda div 
+    { 
+        background: rgba(245, 186, 187, 0.7); 
+        margin-bottom: 10px; 
+        padding: 15px; 
+    }
+    .derecha 
+    { 
+        background: rgba(255, 245, 242, 0.7); 
+        padding: 20px; 
+    }
+  </style>
+</head>
+<body>
+  <div class="container">
+    <br><div class="row">
+      <div class="col panel">
+        <h1>Tepeapulco, Hidalgo</h1>
+      </div>
+    </div><br>
+    
+    <div class="row">
+      
+      <div class="col-5 izquierda">
+        <h2 class="titulo">LUGARES EXTRAORDINARIOS</h2>
+        <div class="col mb-2 uno">
+            <H3>Ex Convento de San Francisco</H3><br>
+            <p>Construcción del siglo XVI, hoy alberga el Museo Arqueológico de 
+              Tepeapulco, con piezas prehispánicas y coloniales. Zona arqueológica 
+              de Xihuingo: Sitio con influencia teotihuacana donde destacan pirámides 
+              y observaciones astronómicas. </p>
+         </div>
+        <br><br><br>
+        <div class="col mb-2 dos">
+            <h3>Cerro Xihuingo</h3>
+            <p>Montaña simbólica y punto de referencia natural, ideal para el senderismo. 
+              En sus laderas se han encontrado restos arqueológicos que refuerzan la conexión 
+              del cerro con la antigua Xihuingo.</p>
+        </div>
+        <br><br><br>
+        <div class="col mb-2 tres">
+          <h3>Laguna de Tecocomulco</h3>
+          <p>Única laguna natural en Hidalgo, considerada sitio Ramsar por su riqueza ecológica. 
+            Es refugio de aves migratorias y hogar de especies como el ajolote. Se practica pesca artesanal 
+            y paseos en lancha.</p>
+        </div>
+        <br><br><br>
+        <div class="col mb-2 tres">
+          <h3>Casa atribuida a Hernán Cortés</h3>
+          <p>Se ubica frente al Ex Convento de San Francisco y fue construida entre 1522 y 1526. Se dice que formó 
+            parte de una encomienda otorgada a Hernán Cortés, utilizada como lugar de descanso cuando viajaba por la 
+            región de los Llanos de Apan.</p>
+        </div>
+        <br>
+      </div>
+      
+      <!-- Columna derecha -->
+      <div class="col-7 derecha">
+        <h2>HISTORIA</h2><br>
+        <p>Tepeapulco es un municipio ubicado en el estado de Hidalgo, dentro de la región de los Llanos de Apan. 
+          Su historia se remonta a tiempos prehispánicos, cuando fue un importante centro de población con vínculos 
+          culturales y comerciales con Teotihuacán.Durante la época colonial, los frailes franciscanos fundaron el 
+          Convento de San Francisco en el siglo XVI, convirtiéndolo en un punto clave para la evangelización. 
+          En esta etapa también se levantó la casa atribuida a Hernán Cortés, lo que refleja la relevancia política 
+          y económica que tuvo el lugar. Hoy en día, Tepeapulco está conformado por más de 40 localidades, entre las 
+          que destacan Ciudad Sahagún, Irolo. El municipio ha tenido 
+          una fuerte contribución industrial, especialmente a través de Ciudad Sahagún, que fue un importante polo 
+          de desarrollo ferroviario, automotriz y de maquinaria pesada durante el siglo XX. Actualmente, su economía 
+          combina actividades industriales, agrícolas y de servicios, además de mantener un papel turístico gracias 
+          a su patrimonio histórico y natural. En la actualidad, Tepeapulco combina su riqueza histórica con atractivos 
+          como la Laguna de Tecocomulco y el Cerro Xihuingo, además de conservar tradiciones, fiestas religiosas y una 
+          gastronomía reconocida dentro del programa “Pueblos con Sabor”, lo que lo coloca como un municipio que aporta 
+          tanto al desarrollo cultural como económico del estado de Hidalgo.<p>
+        <div class="container mt-4">
+
+        <div class="row">
+          
+          <div class="col-md-6 mb-4 text-center">
+            <img src="img/img1.jpg" class="img-fluid rounded shadow" style="height:200px; object-fit:cover; width:100%;" alt="Imagen 1">
+            <p class="mt-2">Emblematicos lavaderos en Tepeapulco</p>
+          </div>
+          
+          <div class="col-md-6 mb-4 text-center">
+            <img src="img/img2.jpg" class="img-fluid rounded shadow" style="height:200px; object-fit:cover; width:100%;" alt="Imagen 2">
+            <p class="mt-2">Zona Arqueológica Xihuingo</p>
+          </div>
+          
+          <div class="col-md-6 mb-4 text-center">
+            <img src="img/img3.jpg" class="img-fluid rounded shadow" style="height:200px; object-fit:cover; width:100%;" alt="Imagen 3">
+            <p class="mt-2">Laguna de Tecocomulco</p>
+          </div>
+          
+          <div class="col-md-6 mb-4 text-center">
+            <img src="img/img4.jpg" class="img-fluid rounded shadow" style="height:200px; object-fit:cover; width:100%;" alt="Imagen 4">
+            <p class="mt-2">Zona industrial en Cuidad Sahagún</p>
+          </div>
+          
+        </div>
+      </div>
+
+      </div>
+    </div><br>
+
+    <div class="row">
+      <div class="col panel">
+        <p>Instituto Tecnológico de Pachuca<br>
+        Hecho por: Jetsamin Bello Gonzalez - 22200715<br>
+         Programación Web<br>
+        Profesor: Luis Alejandro Santana Valadez</p>
+      </div>
+    </div>
+  </div>
+
+    <script src="bootstrap/js/jquery-3.5.1.min.js"></script>
+    <script src="bootstrap/js/popper.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ miempresa/servicioweb/clsservicios.php
@@ -0,0 +1,20 @@
+<?php
+//NOMBRE DE LA CLASE
+class clsservicios{
+    //PROGRAMACIÓN DE MÉTODOS DE LA CLASE
+    
+    //LOS MÉTODOS SERÁN FUNCIONES PUBLICAS, EN ESTE CASO SE RECIBE UN PARÁMETRO
+     public function nombre($nom)
+     {
+         //LOS DATOS QUE SE RETORNAS SON CADENAS O ARREGLOS DE CADENAS 
+         //LOS CUALES PUEDEN SER REGRESADOR COMO ARRAY O ARCHIVOS JSON
+         return "Bienvenido " .$nom .", estás usando el servicio web";
+     }
+     
+     public function nombreUsuario($nom, $usu)
+     {
+        //Los datos que retornara son cadenas o arreglos de cadenas
+        return "Bienvenido " . $nom . " [".$usu."] estas usando el servicio web";
+     }
+}
+?>
